5c04412bad3f107a5bed3fbfca1f8f9156e5780e
Felipe Ferreira <felipesqf@gmail.com>
2020-11-16 17:00:45 +1100

    change buttons

--- config/orm.js
+++ config/orm.js
@@ -70,7 +70,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
