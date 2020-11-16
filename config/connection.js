2cdc1f6bca9b0dc23920320a613d78247107582f
Felipe Ferreira <felipesqf@gmail.com>
2020-11-16 18:07:52 +1100

    changing db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql"); //requirte mysql
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Fel!pe012021",
-  database: "burgers_db"
+  user: "ccfvuei4uw55rgsc",
+  password: "ojisdo7jw7e7bfja",
+  database: "gpg5rd01lex2my5z"
 });
 
 //start connection
